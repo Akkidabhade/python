e868c6cf4135740cc7561c93be8119f533286510
Akkidabhade <akkidabhade0803@gmail.com>
2024-05-09 19:49:18 +0530

    Add files via upload

---
+++ if and else.py
@@ -0,0 +1,107 @@
+
+x = int(input("enter no "))
+enter no 45
+y = int(input("enter no "))
+enter no 23
+if(x>y) :
+    print("number is greater ")
+else:
+    print("number is not greater")
+
+    
+number is greater 
+percentage = float(input("eneter your percentage"))
+eneter your percentage55
+if percentage>=80 :
+    print("O greade ")
+else (percentage>=70) :
+    
+SyntaxError: expected ':'
+if percentage>=80 :
+    print("O greade ")
+elif(percentage>=70) :
+    print("A greade")
+elif(percentage>=60) :
+    print("B greade")
+elif(percentage>=50) :
+    print("C grade")
+elif(percentage>=40) :
+    print("D greade")
+elif(percentage>=30) :
+    print("Fail")
+
+    
+C grade
+per = float (input("enter u r per"))
+enter u r per77.45
+if per>=90 :
+    print("A+ Greade")
+elif(percentage>=80) :
+    print("A Greade")
+
+    
+
+if per>=90 :
+    print("A+ Greade")
+elif(percentage>=80) :
+    print("A Greade")
+else:
+
+    print("fail")
+... 
+...     
+fail
+>>> per = float (input("enter u r per"))
+enter u r per55.6
+>>> if per>=90 :
+...     print("A+ Greade")
+... elif(per>=80) :
+...     print("A greade")
+... elif(per>=70) :
+...     print("B Greade")
+... elif(per>=60) :
+...     print("C greade")
+... elif(per>=50) :
+...     print("D greade")
+... elif(per>=40) :
+...     print("fail")
+... 
+...     
+D greade
+>>> name = "akash"
+>>> print('ak' in name)
+True
+>>> print('ak' not in name)
+False
+>>> x = 30
+>>> y = 30
+>>> w = 20
+>>> print(id(x))
+140722059885912
+>>> print(id(y))
+140722059885912
+>>> print(id(w))
+140722059885592
+>>> print(x is y)
+True
+>>> print(w is x)
+False
+>>> print(w is not y)
+True
+#take a sample string
+info="Learnprogram"
+if info=="Learn" :
+    print("good mor")
+elif info=="Program" :
+    print("good after")
+elif info==("Learning") :
+    print("good eve")
+elif info==("Learnprogram") :
+    print("eee")
+else:
+    print("not match")
+
+
+
+
+    
